74d7a82ce008e9a92619da87be742587eb57085d
Goemay <raihangumay02@gmail.com>
2025-10-09 19:02:16 +0700

    auto-commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/porto.github.io/', // 👈 must match your repo name
+  base: '/porto-/', 
 })
